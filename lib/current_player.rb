8177af2d0bbe2394c2752e67b2a498f29488de10
Matt Pawelski <mxpawels@gmail.com>
2021-06-10 01:09:46 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,1 +1,16 @@
 
+counter = 0;
+
+def turn_count(board)
+  board.each |space| do
+    if(space != " ") 
+      counter += 1
+    end
+  end
+end
+
+def current_player(board)
+  (turn_count(board)%2 = 0) ? "O" : "X"
+end
+
+
